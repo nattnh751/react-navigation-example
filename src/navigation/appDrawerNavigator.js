38dabf6449073478fc59029cb859899cac86b6f1
Nathan Walsh <nwalsh@skillz.com>
2022-01-27 14:16:32 -0800

    updates to be more like the skillz app

--- src/navigation/appDrawerNavigator.js
+++ src/navigation/appDrawerNavigator.js
@@ -1,14 +1,55 @@
 import React from 'react';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { View, Button, Touchable } from 'react-native';
+import {
+    createDrawerNavigator, DrawerHeaderProps, DrawerContentScrollView,
+    DrawerItemList, DrawerItem,
+} from '@react-navigation/drawer';
 import ProfileScreen from '../screens/ProfileScreen'
 import DeepLinkTestScreen from '../screens/DeepLinkTestScreen'
-
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { AllScreenTabNavigator } from "./appTabNavigator";
+import ThirdScreenNavigation from "./appStackNavigator";
 const Drawer = createDrawerNavigator();
 const AllDrawerNavigation = () => (
-    <Drawer.Navigator initialRouteName='Profile' screenOptions={{ drawerPosition: 'right' }}>
-        <Drawer.Screen component={ProfileScreen} name='Profile' />
-        <Drawer.Screen component={DeepLinkTestScreen} name='Deep Link Test' />
+    <Drawer.Navigator initialRouteName='Profile' drawerContent={(props) => <CustomDrawerContent {...props} />} screenOptions={{
+        header: (props: DrawerHeaderProps) => (
+            <View
+                style={{
+                    backgroundColor: 'red',
+                    height: 80,
+                    alignItems: 'right',
+                    width: '100%',
+                }}>
+                <TouchableOpacity
+                    style={{
+                        backgroundColor: 'white',
+                        color: 'black',
+                        height: 80,
+                        width: 40,
+                        marginEnd: 0,
+                    }}
+                    onPress={() => { props.navigation.toggleDrawer() }} />
+            </View>
+        )
+    }}>
+        <Drawer.Screen name="tab" component={AllScreenTabNavigator} />
+        <Drawer.Screen name="stack" component={ThirdScreenNavigation} />
+
     </Drawer.Navigator >
 )
-
+function CustomDrawerContent(props) {
+    return (
+        <DrawerContentScrollView {...props}>
+            <DrawerItemList {...props} />
+            <DrawerItem
+                label="Help"
+                onPress={() => props.navigation.navigate("tab", { screen: "Leagues" })}
+            />
+            <DrawerItem
+                label="Profile"
+                onPress={() => props.navigation.navigate("profile")}
+            />
+        </DrawerContentScrollView>
+    );
+}
 export default AllDrawerNavigation;
--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,24 +1,25 @@
 import React from 'react'
 import { SafeAreaView } from 'react-native';
 import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, useNavigationContainerRef } from "@react-navigation/native";
 import { AllScreenTabNavigator } from "./appTabNavigator";
 import AllDrawerNavigation from "./appDrawerNavigator";
 import ThirdScreenNavigation from "./appStackNavigator";
+import ProfileScreen from '../screens/ProfileScreen'
+
 const AllAppNavigation = createStackNavigator();
 
 const RootNavigator = () => (
     <SafeAreaView style={{ height: '100%', width: '100%' }}>
         <NavigationContainer>
             <AllAppNavigation.Navigator
-                initialRouteName="tab"
+                initialRouteName="drawer"
                 screenOptions={{
                     header: () => null
                 }}
             >
-                <AllAppNavigation.Screen name="tab" children={AllScreenTabNavigator} />
-                <AllAppNavigation.Screen name="stack" children={ThirdScreenNavigation} />
                 <AllAppNavigation.Screen name="drawer" children={AllDrawerNavigation} />
+                <AllAppNavigation.Screen name="profile" children={ProfileScreen} />
             </AllAppNavigation.Navigator>
         </NavigationContainer>
     </SafeAreaView>
